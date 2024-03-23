f5e656f97b6157aa8b166ab2baae5bb0c1a09a3f
sha-lin <shalin.rono@student.moringaschool.com>
2024-03-23 22:48:03 +0300

    create postgresql database

--- budget/settings.py
+++ budget/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mybudgettracker',
+        'USER': 'shalin07',
+        'PASSWORD': 'Chepkoech03',
+        'HOST': 'localhost',
     }
 }
 
