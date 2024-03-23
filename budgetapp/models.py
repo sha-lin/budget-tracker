030c60afa010d747f6c987c8ac1a5a1e15f0ccbe
sha-lin <shalin.rono@student.moringaschool.com>
2024-03-23 21:58:31 +0300

    create budget table and assign fields

--- budgetapp/models.py
+++ budgetapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Budget(models.Model):
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    earnings= models.IntegerField(default=0)
+    name= models.CharField(max_length=150, default='something')
+    price= models.IntegerField(default=0)
